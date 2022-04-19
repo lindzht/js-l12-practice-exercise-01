8129e874b96a4966febdeca88af5167872ceea70
lindzht <llindsayttaylor@gmail.com>
2022-04-18 20:32:49 -0400

    added start of images api

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
 const randomFolks = document.querySelector(".random-peeps");
+
+const getImage = async function(){
+    const res = await fetch("https://picsum.photos/v2/list?limit=100");
+    const images = await res.json();
+    console.log(images);
+}
+
+getImage();
